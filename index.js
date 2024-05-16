db89953f5f144cf0880075cc7c844f57ed81d1a1
Ridwan <ridwanyinusa15@gmail.com>
2024-05-16 15:18:59 +0100

    VV

---
+++ index.js
@@ -0,0 +1,3 @@
+document.querySelector(".hamburger-button").addEventListener("click", function () {
+  document.querySelector(".nav_links").style.display = document.querySelector(".nav_links").style.display == "none" ? "block" : "none";
+});
